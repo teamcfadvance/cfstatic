4660dc64c4799e26efd8aa65d2737b2796b31078
Dominic Watson <watson.dominic@gmail.com>
2012-08-23 23:32:34 +0100

    First working version of ebook generation

---
+++ _ebooks/build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Ensure we run from the correct directory (this one)
+SCRIPT=`readlink -f $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cd $SCRIPTPATH
+
+echo "========================================"
+echo "Copying site pages from gh-pages site..."
+cp ../*.md ./
+echo "...done."
+echo "========================================"
+
+echo "========================================"
+echo "Building pages using jekyll..."
+jekyll
+echo "...done."
+echo "========================================"
+
+echo "========================================"
+echo "Turning into single .html page..."
+
+echo '<html> <head> <title>CfStatic - User Guide</title> <meta name="description" content="CfStatic, CSS and JavaScript control for CFML applications"> <meta name="author" content="Dominic Watson"> <meta http-equiv="Content-Language" content="en"> </head> <body>' > book.html
+echo '<a id="index-html"></a>' >> book.html
+cat _site/index.html >> book.html
+echo '<a id="quick-start-html"></a>' >> book.html
+cat _site/quick-start.html >> book.html
+echo '<a id="full-guide-html"></a>' >> book.html
+cat _site/full-guide.html >> book.html
+echo '<a id="downloads-html"></a>' >> book.html
+cat _site/downloads.html >> book.html
+echo '<a id="contributing-html"></a>' >> book.html
+cat _site/contributing.html >> book.html
+echo "</body> </html>" >> book.html
+echo "...done."
+echo "========================================"
+
+echo "========================================"
+echo "Converting links to be internal..."
+sed -e 's/"[a-z\-]\+\.html#/"#/g' book.html > tmp && mv tmp book.html
+sed -e 's/"\([a-z\-]\+\)\.html/"#\1-html/g' book.html > tmp && mv tmp book.html
+echo "...done."
+echo "========================================"
+
+echo "========================================"
+echo "Building eBooks using Calibre..."
+ebook-convert book.html ./cfstatic.mobi --max-levels=0 --chapter-mark=none --page-breaks-before='//h1' --cover=./cover.png --level1-toc="//h:h1" --level2-toc="//h:h2" --toc-filter="Table of contents" --breadth-first --mobi-toc-at-start
+#ebook-convert ./book.html cfstatic.epub --extra-css ./css/epub.css --use-auto-toc --margin-left 10.0 --margin-right 10.0 --pretty-print --no-default-epub-cover
+#ebook-convert ./book.html cfstatic.pdf  --extra-css ./css/pdf.css  --use-auto-toc --margin-left 10.0 --margin-right 10.0
+echo "...done."
+echo "========================================"
+
+
+echo "========================================"
+echo "Cleaning up generated files..."
+rm ./*.md
+rm ./book.html
+rm -r _site
+echo "...done."
+echo "========================================"
+
+exit 0;
--- downloads.md
+++ downloads.md
@@ -4,11 +4,12 @@ title: Downloads
 ---
 
 # {{ page.title }}
-
+<a id="stable"></a>
 ## Stable release
 
 The latest stable release can be downloaded from the [CfStatic Riaforge project](http://cfstatic.riaforge.org). This release can also be downloaded by choosing the latest [release tag on GitHub](https://github.com/DominicWatson/cfstatic/tags). Choose this for up to date documentation and the most thorough testing.
 
+<a id="development"></a>
 ## Development releases
 
 There are two maintained branches for development work, `master` and `develop`. The `master` branch can be used to try out tested features that might not have been released yet. The `develop` branch is the bleeding edge code that may or may not have been fully tested.
@@ -17,6 +18,7 @@ There are two maintained branches for development work, `master` and `develop`. 
 
 [Download develop](https://github.com/DominicWatson/cfstatic/zipball/develop)
 
+<a id="archived"></a>
 ## Archived releases
 
 You can [view and download from a list of previous releases here](https://github.com/DominicWatson/cfstatic/tags).
