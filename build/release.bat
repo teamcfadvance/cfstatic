51b35d5e0ae50dc4d623f3e7e762667c05d4fdac
Dominic Watson <watson.dominic@gmail.com>
2013-10-12 14:28:37 +0100

    same

--- build/release.bat
+++ build/release.bat
@@ -27,7 +27,7 @@ echo Building documentation using Jekyll and the gh-pages branch
 echo ===========================================================
 call mkdir docs
 call git checkout gh-pages
-echo jekyll build -d %rootdir%\docs
+call jekyll build -d .\docs\
 
 goto done
 
